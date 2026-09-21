be0f2e8628934700f576bbee28e28cf8f6acaf4d
Felix Kiprotich <felixkiprotich2000@gmail.com>
2026-09-21 15:50:56 +0300

    adjusted environment variables

--- app/auth/profile/page.tsx
+++ app/auth/profile/page.tsx
@@ -15,8 +15,8 @@ import {
   EyeOff,
 } from "lucide-react";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
-const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://127.0.0.1:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const WS_URL = process.env.NEXT_PUBLIC_WS_URL;
 
 type UserData = {
   id: number;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,8 +13,8 @@ import {
   Activity,
 } from "lucide-react";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
-const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://127.0.0.1:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+const WS_URL = process.env.NEXT_PUBLIC_WS_URL
 
 type User = {
   username: string;
